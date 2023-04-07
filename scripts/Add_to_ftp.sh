9ad6cefa9018d623496e52ffce3012f5f232f7e4
Jack Tierney <jack.tierney@student.ucc.ie>
2023-04-07 08:51:40 +0100

    added comments re usage

--- scripts/Add_to_ftp.sh
+++ scripts/Add_to_ftp.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
+
+# This script is used to add the miRNA data to the FTP site
+# 
+# Usage: Add_to_ftp.sh <path_to_data>
+
 MIRMACHINE_FTP_PATH="/nfs/production/flicek/ensembl/production/ensemblftp/rapid-release/species"
 PATH_TO_DATA=$($1)
 
