8d6dca8e8c766e2c8bdd11f441ae187327852d16
Jack Tierney <jack.tierney@student.ucc.ie>
2023-04-07 08:49:54 +0100

    added bash script for handling ftp transfer

---
+++ scripts/Add_to_ftp.sh
@@ -0,0 +1,6 @@
+MIRMACHINE_FTP_PATH="/nfs/production/flicek/ensembl/production/ensemblftp/rapid-release/species"
+PATH_TO_DATA=$($1)
+
+sudo -u genebuild mkdir -p ${MIRMACHINE_FTP_PATH}/Rhincodon_typus/GCA_021869965.1/rnaseq/
+sudo -u genebuild rsync -ahvW ${PATH_TO_DATA}/*  ${MIRMACHINE_FTP_PATH}/ 
+sudo -u genebuild find $MIRMACHINE_FTP_PATH -user genebuild -exec chmod g+w {} \;
